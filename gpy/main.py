4d94d52d5c82bba6f315b7a135de0a3375e5b0a8
Gabriele Lanaro <glanaro@seawolf1.ibb>
2015-04-14 15:27:06 -0700

    Added gromacs commands

--- gpy/main.py
+++ gpy/main.py
@@ -2,6 +2,8 @@ import click
 import os
 import glob
 import shutil
+import subprocess
+
 from ConfigParser import SafeConfigParser, DuplicateSectionError
 
 from fabric.api import *
@@ -18,11 +20,12 @@ def init(path=None):
     # All we do is creating boilerplate inside the directory
 
     path = path if path else '.'
-    with cd('path'):
-        conffile = os.path.join(path, '.gpy')
-        if not os.path.exists(conffile):
-            # create
-            os.system('touch %s' % conffile)
+    conffile = os.path.join(path, '.gpy')
+    if not os.path.exists(conffile):
+        # create
+        os.system('touch %s' % conffile)
+    else:
+        click.echo('INFO: already a repository')
 
 @main.command('list')
 @click.argument('path', required=False)
@@ -106,6 +109,104 @@ def config(key, value):
     with open(configfile, 'w') as f:
         config.write(f)
 
+@main.command()
+@click.option('--name', '-n')
+@click.option('--time', '-t')
+@click.option('--cpus', '-c')
+@click.option('--email', '-e')
+@click.option('--yes', is_flag=True)
+@click.option('--no', is_flag=True)
+def qsub(name, time, cpus, email, yes, no):
+    
+    if not _checkproject():
+        return
+
+    template = _get_template('qsub.tpl')
+    config = _conf()
+    
+    if template is None:
+        return
+
+    if email is None:
+        email = _get_conf('email')
+    
+    time = time if time is not None else config.get('qsub', 'time')
+    name = name if name is not None else config.get('qsub', 'name')
+    cpus = cpus if cpus is not None else config.get('qsub', 'cpus')
+    
+    render = template.format(name=name, time=time, cpus=cpus, email=email)
+    with open('sub.pbs', 'w') as fd:
+        fd.write(render)
+    
+    click.echo('WRITTEN: sub.pbs')
+    ret = subprocess.check_output(['qsub', 'sub.pbs'])
+    click.echo('INFO: Created job %s' % ret)
+
+    if not config.has_section('qsub'):
+        config.add_section('qsub')
+    
+    config.set('qsub', 'job', ret)
+
+    if no:
+        pass
+    elif yes or click.confirm('Do you want to save the settings for next time?'):
+        config.set('qsub', 'name', name)
+        config.set('qsub', 'cpus', cpus)
+        config.set('qsub', 'time', time)
+        click.echo('WRITTEN: default settings')
+    
+    with open(config.filename, 'w') as fd:
+        config.write(fd)
+
+@main.command()
+def qdel():
+    config = _conf()
+    job = config.get('qsub', 'job')
+    config.remove_option('qsub', 'job')
+    exitcode = os.system('qdel %s' % job)
+    if exitcode == 0:
+        click.echo(click.style('SUCCESS: Job %s successfully removed.' % job, fg='green'))
+    
+def isroot(dir):
+    return os.path.abspath(dir) == os.path.abspath(os.path.join(dir, '..'))
+
+def _get_template(tplname, path='.'):
+    prev = os.getcwd()
+    if path != None:
+       os.chdir(path)
+
+    tplpath = os.path.join('templates', tplname)
+
+    # Base case 1 -- found
+    if os.path.exists(tplpath):
+       click.echo(click.style('USING: Template %s' % os.path.abspath(tplpath), fg='green'))
+       template = open(tplpath).read()
+    # Base case 2 -- not found
+    elif not _checkproject() or isroot('.'):
+       template = None
+       click.echo('ERROR: Template not found.')
+    # Base case 3 -- recursive case
+    else:
+       template = _get_template(tplname, '..')
+
+    os.chdir(prev)
+    return template
+
+@main.command()
+def minimize():
+    pass
+
+@main.command()
+def compile():
+    # TODO
+    # Just run the command
+    cmd = 'grompp_mpi_d -f eq.mdp -c start.gro -p topol.top'
+    os.system(cmd)
+
+@main.command()
+def run():
+    #TODO
+    os.system('mdrun_mpi_d -v')
 
 def _checkproject():
     if not os.path.exists('.gpy'):
@@ -120,6 +221,16 @@ def _get_conf(key):
     config.read(configfile)
 
     return config.get('main', key)
+
+def _conf():
+    configfile = '.gpy'
+    config = SafeConfigParser()
+    config.read(configfile)
+    
+    config.filename = configfile
+    return config
+
+    
 
 if __name__ == '__main__':
     main()
--- tests/test_qsub.sh
+++ tests/test_qsub.sh
@@ -2,12 +2,17 @@
 mkdir /tmp/testdata1/
 
 cd /tmp/testdata1/
-
 gpy init
+
 mkdir templates
-echo 'Run command for {hours} hours, on {cpus} CPUs, named {name}' > templates/qsub.tpl
+
+echo 'Run command for {time} hours, on {cpus} CPUs, named {name}' > templates/qsub.tpl
 echo 'Send also an email at {email}' >> templates/qsub.tpl
 
-gpy qsub -n 64 -h 120
 
+mkdir proj2
+cd proj2
+gpy init
+gpy qsub --yes -c 64 -t 120 -n 'Job Name'
+gpy qdel
 rm -rf /tmp/testdata1/
