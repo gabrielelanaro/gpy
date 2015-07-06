8b872bea2a6d0497dfbb7b6d4c145b7759bbeb05
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-07-06 10:21:12 -0700

    up

--- gpy/main.py
+++ gpy/main.py
@@ -3,6 +3,7 @@ import os
 import glob
 import shutil
 import subprocess
+import imp
 
 from ConfigParser import SafeConfigParser, DuplicateSectionError
 
@@ -128,12 +129,12 @@ def submit(name, time, cpus, email, genonly):
 
     if email is None:
         email = _get_conf('email')
-    
+
     if not config.has_section('qsub') and not all([time, cpus, name]):
         click.echo('ERROR: No qsub configuration found:')
         click.echo('Please provide options --time --cpus --name')
         return
-     
+
     time = time if time is not None else config.get('qsub', 'time')
     name = name if name is not None else config.get('qsub', 'name')
     cpus = cpus if cpus is not None else config.get('qsub', 'cpus')
@@ -144,14 +145,14 @@ def submit(name, time, cpus, email, genonly):
 
     click.echo('WRITTEN: sub.pbs')
     click.echo('USING: time=%s | name=%s | cpus=%s' % (time, name, cpus))
-    
+
     if not config.has_section('qsub'):
         config.add_section('qsub')
-    
+
     config.set('qsub', 'time', time)
     config.set('qsub', 'name', name)
     config.set('qsub', 'cpus', cpus)
-    
+
     if not genonly:
         ret = subprocess.check_output(['qsub', 'sub.pbs'])
         click.echo('INFO: Created job %s' % ret)
@@ -231,6 +232,23 @@ def status():
     else:
         lf = Logfile.parse('md.log')
         click.echo(repr(lf))
+
+@main.command()
+def shell():
+    '''Open a shell in the remote directory'''
+    if not _checkproject():
+        return
+
+    config = _conf()
+    path = config.get('main', 'remote')
+    host = _get_conf('host')
+    u = _get_conf('user')
+    p = _get_conf('password')
+
+    if not all([host, u, p, path]):
+        click.echo('ERROR: remote, host, user, password configurations required.')
+        return
+    os.system('ssh -t {}@{} "cd {} && bash"'.format(u, host, path))
 
 
 def isroot(dir):
@@ -314,6 +332,7 @@ def _get_template(tplname, path='.'):
 
     os.chdir(prev)
     return template
+
 @main.command()
 def minimize():
     pass
@@ -331,13 +350,71 @@ def run():
     os.system('mdrun_mpi_d -v')
 
 @main.command()
+@click.argument('names', nargs=-1)
+@click.option('-l', is_flag=True)
+def script(names, l):
+    """Run script"""
+
+    fail = False
+    funcs = []
+
+    if l:
+        for f in _find_avail_files('scripts'):
+            if f.endswith('.py'):
+                modname = os.path.splitext(os.path.basename(f))[0]
+                click.echo(click.style('Module name: %s' % modname, fg='green'))
+                _disp_commands(f)
+        return
+
+    for name in names:
+        if ':' in name:
+            mod, function = name.split(':')
+        else:
+            mod = name
+            function = None
+
+        path = _find_file_parent('scripts/%s.py' % mod)
+
+        if path == None:
+            return
+
+        if function == None: # Run normally
+            click.echo('Running the module %s' % mod)
+            os.system('python %s' % path)
+        else:
+            module = imp.load_source(mod, path)
+            # call function
+            try:
+                func = getattr(module, function)
+                funcs.append(func)
+            except AttributeError:
+                click.echo(
+                  click.style('Function "%s" not present in file %s' %
+                              (function, path), fg='red'))
+                _disp_commands(path)
+                fail = True
+    if not fail:
+        [func() for func in funcs]
+
+def _disp_commands(pyfile):
+    mod = os.path.splitext(os.path.basename(pyfile))[0]
+    module = imp.load_source(mod, pyfile)
+    click.echo(click.style('Functions available', bold=True))
+    if hasattr(module, '__all__'):
+        [click.echo(click.style(fun, fg='yellow')) for fun in module.__all__]
+    else:
+        [click.echo(click.style(fun, fg='yellow'))
+        for fun in dir(module)
+        if not fun.startswith('_')]
+
+@main.command()
 @click.option('--deep', '-d', is_flag=True)
 @click.option('--preview', '-p', is_flag=True)
 def clean(deep, preview):
     patterns = ['#*']
     if deep:
         patterns.extend(['traj.xtc', 'traj.trr', 'topol.tpr', 'md.log', 'state*.cpt', 'state.trr'])
-    
+
     torm = sum((glob.glob(pattern) for pattern in patterns), [])
     if preview:
         click.echo('INFO: The following files can be removed:')
@@ -360,6 +437,47 @@ def _get_conf(key):
     config.read(configfile)
 
     return config.get('main', key)
+
+def _find_file_parent(filename, path=None):
+    '''Recurse up to find the filename'''
+    prev = os.getcwd()
+    if path != None:
+       os.chdir(path)
+
+    # Base case 1 -- found
+    if os.path.exists(filename):
+        path_abs = os.path.abspath(filename)
+        click.echo(click.style('USING: File %s' % path_abs, fg='green'))
+    # Base case 2 -- not found
+    elif not _checkproject(mute=True) or isroot('.'):
+        path_abs = None
+        click.echo('ERROR: File not found. Search stopped at: %s' % prev)
+    # Base case 3 -- recursive case
+    else:
+        path_abs = _find_file_parent(filename, '..')
+
+    os.chdir(prev)
+    return path_abs
+
+def _find_avail_files(dirname, path=None):
+    prev = os.getcwd()
+    if path != None:
+       os.chdir(path)
+
+    # Base case 1 -- found
+    if os.path.exists(dirname):
+        paths = [os.path.abspath(os.path.join(dirname, f)) for f in os.listdir(dirname)]
+        paths += _find_avail_files(dirname, '..')
+    # Base case 2 -- not found
+    elif not _checkproject(mute=True) or isroot('.'):
+        paths = []
+        click.echo('ERROR: File not found. Search stopped at: %s' % prev)
+    # Base case 3 -- recursive case
+    else:
+        paths = _find_avail_files(dirname, '..')
+
+    os.chdir(prev)
+    return paths
 
 def _conf():
     configfile = '.gpy'
